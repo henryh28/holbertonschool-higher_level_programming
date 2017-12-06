7216d345a2000883d63d9993a57feda8db44e0ed
Henry Hsu <h_hsu817@yahoo.com>
2017-12-06 00:57:33 +0000

    conver to new print syntax

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
-print(", ".join(["%d%d" % (i, j) for i in range(10) for j in range(10)]))
+print(", ".join(["{:d}{:d}".format(i, j) for i in range(10) for j in range(10)]))
 
 #reference: http://www.diveintopython.net/native_data_types/joining_lists.html
