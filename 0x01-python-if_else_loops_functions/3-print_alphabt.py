89839d953180a4c5d4ac8300be088bb29398e3ac
Henry Hsu <h_hsu817@yahoo.com>
2017-12-06 00:55:16 +0000

    converting to different print style

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 for i in range(ord('a'), ord('z')+1):
     if (i != ord('q') and i != ord('e')):
-        print("%c" % (i), end="")
+        print("{:s}".format(chr(i)), end="")
