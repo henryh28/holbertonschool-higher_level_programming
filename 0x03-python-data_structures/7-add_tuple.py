5e60a0ebc8fd14e3dd9060fad1e431a0d19700ca
Henry Hsu <h_hsu817@yahoo.com>
2017-12-13 03:57:26 +0000

    Fixed index error

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -5,4 +5,4 @@ def add_tuple(tuple_a=(), tuple_b=()):
     new_a = tuple_a + (0, 0)
     new_b = tuple_b + (0, 0)
 
-    return (new_a[0] + new_b[0], new_b[0] + new_b[0])
+    return (new_a[0] + new_b[0], new_a[1] + new_b[1])
