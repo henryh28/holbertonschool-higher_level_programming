83f5a355de09b38adda8d5b9791aa2f9147660e4
Henry Hsu <h_hsu817@yahoo.com>
2017-12-13 03:53:44 +0000

    Reimplement

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,16 +2,7 @@
 
 
 def add_tuple(tuple_a=(), tuple_b=()):
+    new_a = tuple_a + (0, 0)
+    new_b = tuple_b + (0, 0)
 
-    length = min(len(tuple_a), len(tuple_b))
-
-    if (length >= 2):
-        return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
-    elif (length == 1):
-        second = tuple_a[1] if len(tuple_a) > len(tuple_b) else tuple_b[1]
-        return (tuple_a[0] + tuple_b[0], second)
-    elif (length == 0):
-        if (len(tuple_a) > len(tuple_b)):
-            return (tuple_a[0], 0) if (len(tuple_a) < 2) else (tuple_a[0], tuple_a[1])
-        else:
-            return (tuple_b[0], 0) if (len(tuple_b) < 2) else (tuple_b[0], tuple_b[1])
+    return (new_a[0] + new_b[0], new_b[0] + new_b[0])
