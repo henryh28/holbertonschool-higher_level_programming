a7e63e09b2ad571da49a6e1c94535b129045adcd
Henry Hsu <h_hsu817@yahoo.com>
2017-12-13 02:44:17 +0000

    Logic fix

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -5,7 +5,7 @@ def add_tuple(tuple_a=(), tuple_b=()):
 
     length = min(len(tuple_a), len(tuple_b))
 
-    if (length == 2):
+    if (length >= 2):
         return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
     elif (length == 1):
         second = tuple_a[1] if len(tuple_a) > len(tuple_b) else tuple_b[1]
