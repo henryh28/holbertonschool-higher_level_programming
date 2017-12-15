37b992826d68d48ec4e0bd0203ba7aaa9d1fb816
Henry Hsu <h_hsu817@yahoo.com>
2017-12-15 19:46:12 +0000

    Adds check for empty list

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,4 +2,4 @@
 
 
 def best_score(my_dict):
-    return (None if my_dict is None else sorted(my_dict, key=my_dict.get)[-1])
+    return (None if not my_dict else sorted(my_dict, key=my_dict.get)[-1])
