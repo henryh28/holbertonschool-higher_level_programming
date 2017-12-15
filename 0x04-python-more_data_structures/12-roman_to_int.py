2cba56f7f87c6451d2b8578ab3ffc74a10420fda
Henry Hsu <h_hsu817@yahoo.com>
2017-12-15 02:59:12 +0000

    Adds validation checks

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -5,6 +5,9 @@ def roman_to_int(roman_string):
     coins = 0
     roman = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
 
+    if (roman_string is not isinstance(roman_string, str)):
+        return (0)
+
     for i in roman_string:
         if i in roman:
             coins += roman[i]
