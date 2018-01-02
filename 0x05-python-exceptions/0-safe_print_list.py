9150dcf11cbbf426a0a9a2a67dee1082a11319f7
Henry Hsu <h_hsu817@yahoo.com>
2018-01-02 23:57:25 +0000

    Broader exception catches

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,8 +6,8 @@ def safe_print_list(my_list=[], x=0):
 
     for i in range(x):
         try:
-            print(my_list[i], end="")
-        except IndexError:
+            print("{}".format(my_list[i]), end="")
+        except:
             print()
             return (i)
 
