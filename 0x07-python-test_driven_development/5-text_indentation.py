c9fd4f31916b11f159e1d960d5d6098152c4bb7b
Henry Hsu <h_hsu817@yahoo.com>
2018-01-08 19:06:25 +0000

    updated checks

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -10,10 +10,18 @@ def text_indentation(text):
     Outputs text argument verbatim, with the exception of adding
     a newline if it encounters a '.', '?', or ':'
     """
+    leading = True
     if not isinstance(text, str):
         raise TypeError("text must be a string")
 
     for character in text:
-        print (character, end="")
+        if character is " " and text is not " ":
+            if leading is False:
+                print(character, end="")
+        else:
+            print(character, end="")
+            leading = False
+
         if character in ".?:":
+            leading = True
             print("\n")
