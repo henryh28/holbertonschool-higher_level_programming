1e9a19bcba1b2ea5cbe6bdb490bfe4d063b0d6a0
Henry Hsu <h_hsu817@yahoo.com>
2018-03-29 23:28:42 +0000

    Fixed typo

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # -i OPTIONS, -X PUT, -L, provides user id, curl with -d, provide origin
-curl -X PUT -sLd "user_id=98" -H "Origin: Holbertonschool" 0.0.0.0:5000/catch_me
+curl -X PUT -sLd "user_id=98" -H "Origin: HolbertonSchool" 0.0.0.0:5000/catch_me
