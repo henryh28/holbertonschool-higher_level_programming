d9eeef5fe9bd0c903951838141d30e554ae95d8b
Henry Hsu <h_hsu817@yahoo.com>
2018-04-19 20:07:22 +0000

    semistandard fix

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 if (process.argv.length <= 2) {
-    console.log("No argument");
+  console.log('No argument');
 } else {
-    console.log(process.argv.length == 3 ? "Argument found" : "Arguments found");
+  console.log(process.argv.length === 3 ? 'Argument found' : 'Arguments found');
 }
